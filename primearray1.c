557b853a51037ca09588389f64a9e01f55168223
whatvamsii <vamsiwalker711@gmail.com>
2024-08-17 10:25:13 +0530

    Update

---
+++ primearray1.c
@@ -0,0 +1,35 @@
+
+
+
+#include<stdio.h>
+    int prime(int n)
+    
+    {
+        int flag;
+        for(int i=2;i<=n/2;i++)
+        {
+        if(n%i==0)
+        {
+        return 1;
+        }
+        }
+        return 0;
+    }
+int main()
+{
+    int m,index=0, arr[100];
+
+printf("Enter the size of the array: ");
+scanf("%d", &m);
+
+printf("Enter %d elements:\n", m);
+for (int i = 0; i < m; i++) {
+    scanf("%d", &arr[i]);
+}
+for(int j=0;j<m;j++)
+{
+    if(prime(arr[j])==0)
+     index=index+j;
+}
+printf("%d",index);
+}
