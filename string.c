3306d5fd8eb2b3ac67d29bd5c02e98805adba956
whatvamsii <vamsiwalker711@gmail.com>
2024-10-01 20:52:36 +0530

    string.c

--- string.c
+++ string.c
@@ -1,1 +1,28 @@
 // turn wwwrrrttt into w3r3t3                
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[50];
+    printf("Enter a string: ");
+    scanf("%s", s); // No need for & with arrays
+
+    int len = strlen(s);
+    char a = s[0];
+    int count = 1;
+
+    for (int i = 1; i < len; i++) {
+        if (a == s[i]) {
+            count++;
+        } else {
+            printf("%c%d", a, count);
+            a = s[i];
+            count = 1;
+        }
+    }
+
+    // Print the last character and its count
+    printf("%c%d", a, count);
+
+    return 0;
+}
